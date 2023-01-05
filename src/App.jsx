204cb4748def8cc1f65aef2abbda7941cbeee5d7
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2023-01-05 06:15:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me</h1>
+      <Pet name="Luna" animal="dog" breed="havaness" />
+    </div>
+  );
+};
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
